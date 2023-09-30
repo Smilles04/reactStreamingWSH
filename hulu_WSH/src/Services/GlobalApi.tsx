74435a0d6d323c6118d7f926690cb16e71ea5212
William Henao <williamhenao86@outlook.com>
2023-09-30 11:42:57 -0500

    Hulu: se agrega el genero id en la funcion global api

--- hulu_WSH/src/Services/GlobalApi.tsx
+++ hulu_WSH/src/Services/GlobalApi.tsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const movieBaseURL='https://api.themoviedb.org/3/movie';
+const movieByGenreBaseURL='https://api.themoviedb.org/3/movie/550?api_key=0248c5cce04acce18a0fe24a61da1f88';
+
 const api_key= '0248c5cce04acce18a0fe24a61da1f88'
 
 const getPopularMovies=axios.get(movieBaseURL+"/popular?api_key="+api_key)
+const getMovieByGenreId=(id:number)=>axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
 export default{
     getPopularMovies
+    getMovieByGenreId
 }
