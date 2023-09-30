51eb6feaa341fbd22ee82b73a805b8ebfac2f17f
William Henao <williamhenao86@outlook.com>
2023-09-30 01:04:29 -0500

    Hulu: Se agrega lista y logo

--- hulu_WSH/src/Components/Header.tsx
+++ hulu_WSH/src/Components/Header.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import logo from '../assets/Images/logo.png'
 
-function s() {
+function Header() {
+    const menu=[
+            {
+                id:1,
+                name:'Home'
+            },
+            {
+                id:2,
+                name:'Movies'
+            },
+            {
+                id:3,
+                name:'TV'
+            },
+            {
+                id:4,
+                name:'Sports'
+            },
+            {
+                id:5,
+                name:'News'
+            },
+            {
+                id:6,
+                name:'Hubs'
+            },
+    ]
+
   return (
-    <div>header</div>
+    <div>
+        <img src={logo} className="w-[65px]"/>
+        <ul>
+            {menu.map((item)=>(
+                <li className='text-gray-400 text-[18px]
+                font-medium cursor-pointer'>{item.name}</li>
+            ))}
+        </ul>
+
+
+    </div>
   )
 }
 
-export default s
+export default Header
