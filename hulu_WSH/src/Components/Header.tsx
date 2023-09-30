caa98c68090f9ebe3ba631dfbd24911b84c52f4d
William Henao <williamhenao86@outlook.com>
2023-09-30 01:24:59 -0500

    Hulu: se agrega paquete de icono y se usa icono buscar

--- hulu_WSH/src/Components/Header.tsx
+++ hulu_WSH/src/Components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/Images/logo.png'
+import {IoSearch} from "react-icons/io5";
 
 function Header() {
     const menu=[
@@ -30,9 +31,9 @@ function Header() {
     ]
 
   return (
-    <div>
+    <div className='flex'>
         <img src={logo} className="w-[65px]"/>
-        <ul className='flex'>
+        <ul className='flex gap-8'>
             {menu.map((item)=>(
                 <li className='text-gray-400 text-[18px]
                 font-medium cursor-pointer hover:bg-gray-700
@@ -40,6 +41,11 @@ function Header() {
                 rounded-md transition-all duration-500 ease-in-out '>{item.name}</li>
             ))}
         </ul>
+        <div>
+            <IoSearch className='text-[35px] text-gray-300
+            hover:bg-gray-700 px-[3px] pb-[2px] py-[2px]
+            rounded-md hover:text-white transition-all duration-500 ease-in-out'/>
+        </div>
 
 
     </div>
