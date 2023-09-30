fb58e0939ffdf49bae1553fc2e378a8c7abccca5
William Henao <williamhenao86@outlook.com>
2023-09-30 10:41:21 -0500

    Hulu: se agrega los botones y efecto gradient en la imagen

--- hulu_WSH/src/Components/Hero.tsx
+++ hulu_WSH/src/Components/Hero.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 import GlobalApi from '../Services/GlobalApi'
 
 function Hero() {
-
-    const [movieList,setMovieList]=useState([])
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";    
+    const [movieList,setMovieList]=useState<any>([])
 
     useEffect(()=>{
         getPopularMovies();
@@ -13,12 +13,29 @@ function Hero() {
     const getPopularMovies=()=>{
         GlobalApi.getPopularMovies.then((resp:any)=>{
             const result=resp.data.results;
-            setMovieList(result)
+            setMovieList(result[0])
 
         })
     }
   return (
-    <div>Hero</div>
+    <div>
+        <div className='absolute h-[85vh] bg-gradient-to-t
+         from-[#1e2126] via-transparent to-transparent w-full'></div>
+        <div className='absolute mt-[400px] md:mt[350px]
+        px-10 md:px-24'>
+            
+            <h2 className='text-white text-[19px] 
+            lg:text-[27px]'>Watch only on Hulu</h2>
+            <h2 className='text-white text-[36px]
+            lg:text-[47px] font-extrabold'>{movieList.original_title}</h2>
+            <div className='flex gap-5 mt-5'>
+                <button>PLAY</button>
+                <button className='bg-transparent border-2 border-white
+                text-0white cursor-pointer hover:border-gray-500'>DETAILS</button>
+            </div>
+        </div>
+        <img src={IMAGE_BASE_URL+movieList.backdrop_path} width={1920} height={1080} className="h-[85vh] object-cover"/>
+    </div>
   )
 }
 
